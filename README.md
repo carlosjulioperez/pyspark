d376ef43a0f4949b71fc09be7b34d49df647f6d5
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2025-04-27 16:29:52 -0300

    readme y ejemplo con RDD

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pyspark
 PySpark code samples
+
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install pyspark
+
+Exportar dependencias:
+$ pip freeze > requirements.txt
+
+Instalar dependencias:
+$ pip install -r requirements.txt
--- example.py
+++ example.py
@@ -1,10 +1,40 @@
 from pyspark.sql import SparkSession
+import sys
 
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("Examples") \
     .getOrCreate()
 
-rdd = spark.sparkContext.parallelize([("a", 1),("b", 1),("a", 1)])
+sc = spark.sparkContext
+numbRDD = sc.parallelize([1, 2, 3, 4])
+
+# Create map() transformation to cube numbers
+cubedRDD = numbRDD.map(lambda x: x ** 3)
+
+# Collect the results
+numbers_all = cubedRDD.collect()
+
+# Print the numbers from numbers_all
+for numb in numbers_all:
+	print(numb)
+
+# CJ additions:
+# List comprehension
+[print(numb) for numb in numbers_all]
+
+# Using map function
+list(map(print, numbers_all))
+
+# Don't use print for millions of times
+sys.stdout.write('\n'.join(map(str, numbers_all)) + '\n')
+
+#map(str, numbers_all): converts all numbers to strings without creating a list immediately.
+#'\n'.join(...): joins them into one big string.
+#Then only one write happens.
+
+# This is way faster because you're not making a system call (write) for every number.
+
+rdd = sc.parallelize([("a", 1),("b", 1),("a", 1)])
 for (kee, val) in rdd.countByKey().items():
     print(kee, val)
